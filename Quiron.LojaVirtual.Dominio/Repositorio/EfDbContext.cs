35e7a4d7f4583e67158dc676e990ddfe9021833d
MarcosMota <marcos.mota287@gmail.com>
2015-05-29 08:23:17 -0300

    4Versao

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+   public class EfDbContext:DbContext
+    {
+       public EfDbContext() { }
+
+       public DbSet<Produto> Produtos { get; set; }
+
+       protected override void OnModelCreating(DbModelBuilder modelBuilder)
+       {
+           modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+           modelBuilder.Entity<Produto>().ToTable("Produtos");
+       }
+    }
+}
